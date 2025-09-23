a833b9850a74b64c191e66d32774d3449ae74cb6
hieu minh nguyen <22070897@vnu.edu.vn>
2025-09-23 15:12:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,21 @@
 </head>
 <body>
     <?php
-        $x = 10; 
-        $y = 11; 
-        echo "x == y: " . ($x == $y) . "<br/>"; 
-        echo "x != y: " . ($x != $y) . "<br/>"; 
-        echo "x < y: " . ($x < $y) . "<br/>"; 
-        echo "x > y " . ($x > $y) . "<br/>"; 
-        echo "x <= y: " . ($x <= $y) . "<br/>"; 
-        echo "x >= y: " . ($x >= $y) . "<br/>"; 
+            $x = 10; 
+            $y = 11; 
+            echo "x == y: " . ($x == $y) . "<br/>"; 
+            echo "x != y: " . ($x != $y) . "<br/>"; 
+            echo "x < y: " . ($x < $y) . "<br/>"; 
+            echo "x > y " . ($x > $y) . "<br/>"; 
+            echo "x <= y: " . ($x <= $y) . "<br/>"; 
+            echo "x >= y: " . ($x >= $y) . "<br/>"; 
+
+            //https:localhost:8000/?x=5&y=3
+            $x = $_GET["x"]; 
+            $y = $_GET["y"]; 
+
+            echo "x + y = " . ($x + $y) . "<br>"; 
+            echo "x == y: " . ($x == $y) . "<br>";
 
     ?>
 </body>
